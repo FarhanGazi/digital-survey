d89f42947aa07a8cf7b99ae98e4ae4b8d1d1ce90
Farhan Gazi <farhan.gazi@opinioni.net>
2021-07-15 19:14:25 +0200

    NEW: craeted sqlalchemy session with sessionmaker

--- configs/sqladb.py
+++ configs/sqladb.py
@@ -1,7 +1,9 @@
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from configs.config import Config
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(Config.database_url(), echo=True)
-
 Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
--- ds/__init__.py
+++ ds/__init__.py
@@ -1,11 +1,16 @@
+from os import name
 from flask import Flask
 
-from configs.sqladb import Base, engine
+from configs.sqladb import Base, engine, session
 from ds.models.user import *
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     Base.metadata.create_all(engine)
+
+    # new_user = User(name='Gino', surname='Buonvino', email='ginobuonvino.com', password='123', role='Admin')
+    # session.add(new_user)
+    # session.commit()
 
     @app.route('/hello')
     def hello():

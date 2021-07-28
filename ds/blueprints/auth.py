c67bba26c7a614f26447e4bf17caa2fb368b5626
Farhan Gazi <farhan.gazi@opinioni.net>
2021-07-28 20:42:44 +0200

    NEW: added favicon

--- ds/blueprints/auth.py
+++ ds/blueprints/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, request, render_template, redirect, url_for
 from flask_login import login_user
 
 from ds.models.user import User
@@ -7,11 +7,19 @@ from configs.sqladb import DB
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 @bp.route('/signin', methods=['GET', 'POST'])
-def login():
-    if request.method == "GET":
+def signin():
+    if request.method == "POST":
+        #return render_template("auth/signin.html")
+        print(request.form)
+        email = request.form["email"]
+        password = request.form["password"]
+        db = DB('ds')
+        user = db.session.query(User).filter(
+            User.email == email, User.password == password).first()
+        if user:
+            login_user(user)
+            return redirect(url_for("admin"))
+        else:
+            return redirect(url_for("auth.signin"))
+    else:
         return render_template("auth/signin.html")
-
-    # db = DB('ds')
-    # user = db.session.query(User).filter(User.id == 1).first()
-    # login_user(user)
-    # return

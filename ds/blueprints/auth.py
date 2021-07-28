2dca08c213bce46b777affbae2afc88fd752f7d3
doriafederico <federico.doria@outlook.it>
2021-07-28 17:26:21 +0200

    NEW: first try to login from web (not working)

--- ds/blueprints/auth.py
+++ ds/blueprints/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_user
 
 from ds.models.user import User
@@ -8,15 +8,25 @@ bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
 
-@bp.route('/login', methods=['GET', 'POST'])
-def login():
-    db = DB('ds')
-    user = db.session.query(User).filter(User.id == 1).first()
-    login_user(user)
-    return 'LOGGATO!'
+# @bp.route('/login', methods=['GET', 'POST'])
+# def login():
+#     db = DB('ds')
+#     user = db.session.query(User).filter(User.id == 1).first()
+#     login_user(user)
+#     return render_template("homepage.html")
 
 
 
-@bp.route('/home')
-def home():
-    return render_template("index.html")
+@bp.route('/signin', methods=['GET', 'POST'])
+def signin():
+    email = request.form["emailAddress"]
+    if request.method == "POST" and email == "aaaa" :
+        # email = request.form["emailAddress"]
+        # password = request.form["password"]
+        # db = DB('ds')
+        # user = db.session.query(User).filter(User.email == email, User.password == password).first()
+        # login_user(user)
+        return render_template("homepage.html")
+    else:
+        return render_template("signin.html")    
+

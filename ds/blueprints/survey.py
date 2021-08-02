63a15998b7fac2da05a8e3eca3971748f31f6cf8
Farhan Gazi <farhan.gazi@opinioni.net>
2021-08-02 20:33:31 +0200

    NEW: implemented templates for surveys CRUD

--- ds/blueprints/survey.py
+++ ds/blueprints/survey.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, render_template, redirect, url_for
 from flask_login import current_user, login_required
-from sqlalchemy.sql.functions import user
 
 from ds.helpers.auth import requires_roles
 from ds.models.user import User
@@ -13,7 +12,9 @@ bp = Blueprint("survey", __name__, url_prefix="/surveys")
 @login_required
 @requires_roles('admin')
 def list():
-  return render_template("admin/surveys/list.html")
+  db = DB('ds')
+  surveys = db.session.query(Survey).order_by(Survey.id.asc()).all()
+  return render_template("admin/surveys/list.html", surveys=surveys)
 
 
 @bp.route('/<int:id>', methods=['GET'])
@@ -21,7 +22,7 @@ def list():
 @requires_roles('admin')
 def details(id):
     db = DB('ds')
-    survey = db.session.query(Survey).filter(Survey.id == id, Survey.user_id == current_user.id).first()
+    survey = db.session.query(Survey).filter(Survey.id == id).first()
     return render_template("admin/surveys/details.html", survey=survey)
 
 
@@ -31,14 +32,46 @@ def details(id):
 def create():
   if request.method == 'POST':
     title = request.form["title"]
+    status = request.form["status"]
     description = request.form["description"]
-
     db = DB('ds')
-    new_surevy = Survey(title=title, description=description, user_id=current_user.id)
+    new_surevy = Survey(title=title, description=description, status=status, user_id=current_user.id)
     db.session.add(new_surevy)
     db.session.commit()
-
     return redirect(url_for("survey.details", id=new_surevy.id))
 
   elif request.method == 'GET':
     return render_template("admin/surveys/create.html")
+
+
+@bp.route('/<int:id>/update', methods=['GET', 'POST'])
+@login_required
+@requires_roles('admin')
+def update(id):
+  if request.method == 'POST':
+    title = request.form["title"]
+    status = request.form["status"]
+    description = request.form["description"]
+    db = DB('ds')
+    survey = db.session.query(Survey).filter(Survey.id == id).first()
+    survey.title = title
+    survey.status = status
+    survey.description = description
+    db.session.commit()
+    return redirect(url_for("survey.details", id=survey.id))
+
+  elif request.method == 'GET':
+    db = DB('ds')
+    survey = db.session.query(Survey).filter(Survey.id == id).first()
+    return render_template("admin/surveys/update.html", survey=survey)
+
+
+@bp.route('/<int:id>/delete')
+@login_required
+@requires_roles('admin')
+def delete(id):
+  db = DB('ds')
+  survey = db.session.query(Survey).filter(Survey.id == id).first()
+  db.session.delete(survey)
+  db.session.commit()
+  return redirect(url_for("survey.list"))

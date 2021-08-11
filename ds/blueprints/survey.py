6200c476588bacb1593c853911c4f6afc0a9ee3f
Farhan Gazi <farhan.gazi@opinioni.net>
2021-08-11 18:55:56 +0200

    NEW: added completed fillings in survey details

--- ds/blueprints/survey.py
+++ ds/blueprints/survey.py
@@ -1,3 +1,4 @@
+from ds.models.filling import Filling
 from flask import Blueprint, request, render_template, redirect, url_for
 from flask_login import current_user, login_required
 
@@ -8,13 +9,14 @@ from configs.sqladb import DB
 
 bp = Blueprint("survey", __name__, url_prefix="/surveys")
 
+
 @bp.route('/', methods=['GET'])
 @login_required
 @requires_roles('admin')
 def list():
-  db = DB('ds')
-  surveys = db.session.query(Survey).order_by(Survey.id.asc()).all()
-  return render_template("admin/surveys/list.html", surveys=surveys)
+    db = DB('ds')
+    surveys = db.session.query(Survey).order_by(Survey.id.asc()).all()
+    return render_template("admin/surveys/list.html", surveys=surveys)
 
 
 @bp.route('/<int:id>', methods=['GET'])
@@ -23,6 +25,10 @@ def list():
 def details(id):
     db = DB('ds')
     survey = db.session.query(Survey).filter(Survey.id == id).first()
+    completed_fillings = db.session.query(Filling.user_id).filter(
+        Filling.survey_id == id, Filling.status == 'completed').group_by(Filling.user_id).count()
+    survey.completed_fillings = completed_fillings
+
     return render_template("admin/surveys/details.html", survey=survey)
 
 
@@ -30,51 +36,52 @@ def details(id):
 @login_required
 @requires_roles('admin')
 def create():
-  if request.method == 'POST':
-    title = request.form["title"]
-    status = request.form["status"]
-    description = request.form["description"]
-    db = DB('ds')
-    new_surevy = Survey(title=title, description=description, status=status, user_id=current_user.id)
-    db.session.add(new_surevy)
-    db.session.commit()
-    return redirect(url_for("survey.details", id=new_surevy.id))
+    if request.method == 'POST':
+        title = request.form["title"]
+        status = request.form["status"]
+        description = request.form["description"]
+        db = DB('ds')
+        new_surevy = Survey(title=title, description=description,
+                            status=status, user_id=current_user.id)
+        db.session.add(new_surevy)
+        db.session.commit()
+        return redirect(url_for("survey.details", id=new_surevy.id))
 
-  elif request.method == 'GET':
-    return render_template("admin/surveys/create.html")
+    elif request.method == 'GET':
+        return render_template("admin/surveys/create.html")
 
 
 @bp.route('/<int:id>/update', methods=['GET', 'POST'])
 @login_required
 @requires_roles('admin')
 def update(id):
-  if request.method == 'POST':
-    title = request.form["title"]
-    status = request.form["status"]
-    description = request.form["description"]
-    db = DB('ds')
-    survey = db.session.query(Survey).filter(Survey.id == id).first()
-    survey.title = title
-    survey.status = status
-    survey.description = description
-    db.session.commit()
-    return redirect(url_for("survey.details", id=survey.id))
+    if request.method == 'POST':
+        title = request.form["title"]
+        status = request.form["status"]
+        description = request.form["description"]
+        db = DB('ds')
+        survey = db.session.query(Survey).filter(Survey.id == id).first()
+        survey.title = title
+        survey.status = status
+        survey.description = description
+        db.session.commit()
+        return redirect(url_for("survey.details", id=survey.id))
 
-  elif request.method == 'GET':
-    db = DB('ds')
-    survey = db.session.query(Survey).filter(Survey.id == id).first()
-    return render_template("admin/surveys/update.html", survey=survey)
+    elif request.method == 'GET':
+        db = DB('ds')
+        survey = db.session.query(Survey).filter(Survey.id == id).first()
+        return render_template("admin/surveys/update.html", survey=survey)
 
 
 @bp.route('/<int:id>/delete')
 @login_required
 @requires_roles('admin')
 def delete(id):
-  db = DB('ds')
-  survey = db.session.query(Survey).filter(Survey.id == id).first()
-  db.session.delete(survey)
-  db.session.commit()
-  return redirect(url_for("survey.list"))
+    db = DB('ds')
+    survey = db.session.query(Survey).filter(Survey.id == id).first()
+    db.session.delete(survey)
+    db.session.commit()
+    return redirect(url_for("survey.list"))
 
 ###################################################################################
 # DATA EXPORT QUERY

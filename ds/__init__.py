6d16e2327d15f0b4eadc39e8a118cc873b0bae8e
doriafederico <federico.doria@outlook.it>
2021-08-20 16:14:19 +0200

    NEW: working

--- ds/__init__.py
+++ ds/__init__.py
@@ -37,56 +37,6 @@ def create_app():
 
     # Register all blueprints
     app.register_blueprint(authentication.bp)
-<<<<<<< HEAD
-
-    # new_user = User(name='Gino', surname='Buonvino', email='gino@buonvino.com', password='123', role='admin')
-    # db.session.add(new_user)
-    # db.session.commit()
-    # user = db.session.query(User).filter(User.id==1).first()
-
-    # print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
-    # print(user)
-
-    # @app.route('/create', methods=['GET', 'POST'])
-    # def create():
-    #     new_user = User(name='Gino', surname='Buonvino',
-    #                     email='gino@buonvino.com', password='123', role='admin')
-    #     db.session.add(new_user)
-    #     db.session.commit()
-    #     new_survey = Survey(title='Survey1', description='Desc...', status='active', user_id=new_user.id)
-    #     db.session.add(new_survey)
-    #     db.session.commit()
-    #     return 'FATTO!'
-
-    @app.route('/delete', methods=['GET', 'POST'])
-    def delete():
-        user = db.session.query(User).filter(User.id == 2).first()
-        surveys = user.surveys
-        print(surveys)
-        #survey = db.session.query(Survey).filter(Survey.user_id == user.id).first
-        db.session.delete(user)
-        db.session.commit()
-        return surveys[0].title
-
-    @app.route('/logout')
-    @login_required
-    def logout():
-        logout_user()
-        return 'LOGOUT!'
-
-    @app.route('/hello')
-    def hello():
-        if current_user.is_authenticated:
-            print(current_user)
-            return 'AUTHATO!'
-        return 'Hello, World!'
-
-    @app.route('/admin')
-    @login_required
-    @requires_roles('admin')
-    def admin():
-        return 'ADMIN!'
-=======
     app.register_blueprint(survey.bp)
     app.register_blueprint(question.bp)
     app.register_blueprint(answer.bp)
@@ -95,6 +45,5 @@ def create_app():
     app.register_blueprint(filling.bp)
 
     app.add_url_rule("/", endpoint="auth.index")
->>>>>>> c98811db650ae2ddba4619e10e61d0fb3a16bdd4
 
     return app
